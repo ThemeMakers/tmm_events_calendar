a1bd8290a1e565eeda5f0268ddfccaf889dd14a6
Stanislav Samoilenko <st.prog.1986@gmail.com>
2015-03-18 17:11:51 +0200

    events plugin version change from 1.0 to 1.0.0

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
  * Description: Events calendar, events list
  * Author: ThemeMakers
  * Author URI: http://themeforest.net/user/ThemeMakers
- * Version: 1.0
+ * Version: 1.0.0
 */
 
 define('TMM_EVENTS_PLUGIN_PATH', plugin_dir_path(__FILE__));
