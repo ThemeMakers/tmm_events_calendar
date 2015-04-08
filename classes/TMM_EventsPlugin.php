6e582ee891bdec7c2d1c8680e2462f69761ea814
Stanislav Samoilenko <st.prog.1986@gmail.com>
2015-04-08 15:03:31 +0300

    dip-185 Data validation issues

--- classes/TMM_EventsPlugin.php
+++ classes/TMM_EventsPlugin.php
@@ -253,9 +253,9 @@ class TMM_EventsPlugin {
 		wp_enqueue_script('events_js', TMM_EVENTS_PLUGIN_URI . 'js/admin.js');
 		?>
 		<script type="text/javascript">
-			var error_fetching_events = "<?php _e("there was an error while fetching events!", TMM_EVENTS_PLUGIN_TEXTDOMAIN) ?>";
-			var lang_time = "<?php _e("Time", TMM_EVENTS_PLUGIN_TEXTDOMAIN) ?>";
-			var lang_place = "<?php _e("Place", TMM_EVENTS_PLUGIN_TEXTDOMAIN) ?>";
+			var error_fetching_events = "<?php echo esc_js( __("There was an error while fetching events!", TMM_EVENTS_PLUGIN_TEXTDOMAIN) ); ?>";
+			var lang_time = "<?php echo esc_js( __("Time", TMM_EVENTS_PLUGIN_TEXTDOMAIN) ); ?>";
+			var lang_place = "<?php echo esc_js( __("Place", TMM_EVENTS_PLUGIN_TEXTDOMAIN) ); ?>";
 		</script>
 		<?php
 	}
