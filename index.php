d4d75eafc2f742fdeac1bf81022f8c7fa54c7ccf
Oleksiy Ostapenko <lelik4w@gmail.com>
2016-08-01 21:41:07 +0200

    update pluginversion; update language files

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
  * Description: Events calendar, events list
  * Author: ThemeMakers
  * Author URI: http://themeforest.net/user/ThemeMakers
- * Version: 1.0.7
+ * Version: 1.0.8
  * Text Domain: tmm_events
 */
 
